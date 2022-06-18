11cb579775caaea15ba2d0a2fb174d95aa82ae86
Ivan Lytvynenko <ivan.lytvynenko.dev@gmail.com>
2022-06-19 01:17:13 +0200

    Organizing data with Start: part 2
    Embedding Structs

--- structs/main.go
+++ structs/main.go
@@ -2,29 +2,26 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName string
+	contact contactInfo
 }
 
 func main() {
-	// First way to declare structure, don't use it
-	alex := person{"Alex", "Anderson"}
-	fmt.Println(alex)
+	jim := person{
+		firstName: "Jim",
+		lastName: "Party",
+		contact: contactInfo{
+			email: "jim@example.com",
+			zipCode: 94000,
+		},
+	}
 
-	// Second way to declare structure
-	john := person{firstName: "John", lastName: "Smith"}
-	fmt.Println(john)
-
-	// Third way to declare structure
-	// in this case alice is created with zero values for firstName and lastName
-	var alice person
-
-	fmt.Println(alice)
-	fmt.Printf("%+v", alice)
-
-	alice.firstName = "Alice"
-	alice.lastName = "Wilson"
-	fmt.Println(alice)
-	fmt.Printf("%+v", alice)
+	fmt.Printf("%+v", jim)
 }
