5a41ca1fd32fef756e43cf2ed17c094c27e0c41d
Ivan Lytvynenko <ivan.lytvynenko.dev@gmail.com>
2022-06-19 17:04:38 +0200

    Maps: Iterating over maps

--- map/main.go
+++ map/main.go
@@ -3,26 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	// First way to declare map
 	colors := map[string]string{
 		"red": "#FF0000",
 		"green": "#00FF00",
+		"white": "#000000",
 	}
 
-	fmt.Println(colors)
+	printColors(colors)
+}
 
-	delete(colors, "green")
-
-	fmt.Println(colors)
-
-	// Second way to declare map
-	darkColors := make(map[string]string)
-	darkColors["black"] = "#000000"
-
-	fmt.Println(darkColors)
-
-	// Third way to declare map
-	var rainbowColors map[string]string
-
-	fmt.Println(rainbowColors)
+func printColors(c map[string]string) {
+	for color, hex := range c{
+		fmt.Println("Hex color for", color, "is", hex)
+	}
 }
