236e1058db25036c703e91c1caef12e2bc8b82e8
Ivan Lytvynenko <ivan.lytvynenko.dev@gmail.com>
2022-06-21 13:11:15 +0200

    Interfaces: implement the simplest task with interfaces #1

---
+++ interfaces_area_task/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+
+type shape interface{
+	getArea() float64
+}
+
+type square struct{
+	sideLenght float64
+}
+type triangle struct{
+	base float64
+	height float64
+}
+
+func main() {
+	sq := square{sideLenght: 5.05}
+	tr := triangle{base: 3.78, height: 4.05}
+	
+	pritnArea(sq)
+	pritnArea(tr)
+}
+
+func (t triangle) getArea() float64 {
+	return 0.5 * t.base * t.height
+}
+
+func (s square) getArea() float64 {
+	return s.sideLenght * s.sideLenght
+}
+
+func pritnArea(s shape) {
+	fmt.Println("Square is - ", s.getArea())
+}
+
